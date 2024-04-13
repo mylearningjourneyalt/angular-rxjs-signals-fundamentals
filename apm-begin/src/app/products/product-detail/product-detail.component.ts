b1d7b82296151833a674f04abbcbdc61302a33a4
mylearningjourneyalt <mylearningjourneyalt@outlook.com>
2024-04-13 16:14:49 -0400

    Before Declarative Approach Module 8

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,24 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, inject } from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { EMPTY, Subscription, catchError } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges, OnInit, OnDestroy {
   // Just enough here for the template to compile
   @Input() productId: number = 0;
   errorMessage = '';
+
+  private http = inject(ProductService)
 
   // Product to display
   product: Product | null = null;
 
   // Set the page title
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+
+  sub!: Subscription;
+  ngOnInit(): void {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.sub = this.http.getProduct(id).pipe(catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })).subscribe({
+        next: (res: Product) => this.product = res,
+        complete: () => console.log('Product received, complete')
+      })
+    }
+  }
+  ngOnDestroy(): void {
+    //Due to subscription in ngOnChanges
+    //It is possible that his component is Destroy without ever getting a change
+    //By default Prouct is null and the subscription only get set once this component receives an id
+    if (this.sub) {
+      this.sub.unsubscribe()
+
+    }
+  }
 
   addToCart(product: Product) {
   }
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +13,73 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   // Just enough here for the code to compile
   private productsUrl = 'api/products';
+
+  //For constructor to treat http parameter as part of dependency inject we use private
+  // constructor(private http: HttpClient){}
+  //NEW WAY 
+
+  private http = inject(HttpClient);
+  private httpErrorService = inject(HttpErrorService)
+  private reviewService = inject(ReviewService)
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(res => console.log(res, 'In http.get pipeline')),
+      catchError(err => {
+        console.error(err);
+        return this.handleError(err)
+      })
+    )
+  }
+
+  readonly products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    tap(res => console.log(res, 'In http.get pipeline')),
+    catchError(err => {
+      console.error(err);
+      return this.handleError(err)
+    })
+  )
+
+
+
+
+  //Any Observable that emits an observable is call a Higher-order observable
+  //When you want to modify an the result from an htpp request by/with the result from another http request use Higher Order Mapping Operators
+  //This will subscribe/unsubscribe as well as unwrap the result (not returning an observable but actual result)
+
+
+  getProduct(id: number): Observable<Product> {
+    const productUrl = this.productsUrl + '/' + id;
+    return this.http.get<Product>(productUrl).pipe(
+      tap(() => console.log('In http.get single product pipeline')),
+      switchMap(product => this.getProductWithReview(product)),//transformation operator!!! just like map, concatMap, mergeMap
+      catchError(err => {
+        return this.handleError(err)
+      })
+    )
+  }
+
+  private getProductWithReview(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id)).pipe(
+        map(reviews => ({ ...product, reviews })
+        )
+      )
+    }
+    return of(product);
+  }
+
+  //Test Errors by messing with URL
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    //never: an Observable that does not emit anything and does not complete
+    //this way the http request returns an Observable
+
+    const formattedMessage = this.httpErrorService.formatError(err);
+    //creates a replace observable so when you subscription
+    //emits error notification and error message
+    return throwError(() => formattedMessage);
+    // throw formattedMessage;
+
+  }
 
 }
